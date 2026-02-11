d634041fbe4a0ea605911a799013b59e65a1f2c2
victorspg <165102012+victorspg@users.noreply.github.com>
2026-02-10 20:15:35 -0600

    Sales Pipeline created and Sales NB & VL updated.

--- Sales/Sales_NB.Notebook/notebook-content.py
+++ Sales/Sales_NB.Notebook/notebook-content.py
@@ -14,6 +14,9 @@
 # META       "known_lakehouses": [
 # META         {
 # META           "id": "eed7a883-5793-4b88-9eed-8f4ef9968e0f"
+# META         },
+# META         {
+# META           "id": "650f0470-1a95-4792-b6f7-499a03443957"
 # META         }
 # META       ]
 # META     }
@@ -22,9 +25,39 @@
 
 # CELL ********************
 
-df = spark.read.format("csv").option("header","true").load("Files/sales/sales.csv")
-# df now is a Spark DataFrame containing CSV data from "Files/sales/sales.csv".
+# MAGIC %%configure
+# MAGIC {
+# MAGIC   "defaultLakehouse": {
+# MAGIC     "name": { "parameterName": "lakehouse_name", "defaultValue": "<lakehouse_name>" },
+# MAGIC     "id": { "parameterName": "lakehouse_id", "defaultValue": "<lakehouse_id>" },
+# MAGIC     "workspaceId": { "parameterName": "workspace_id", "defaultValue": "<workspace_id>" },
+# MAGIC   }
+# MAGIC }
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load(f"abfss://{workspace_name}@onelake.dfs.fabric.microsoft.com/Sales_LH.Lakehouse/Files/sales/sales.csv")
+
 display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("CREATE SCHEMA IF NOT EXISTS sales_schema")
 
 # METADATA ********************
 
